89cf4c027b810d63cf930213e09bcb58b6ae8470
jpcasa29 <juanpablocasa.lca@gmail.com>
2020-07-06 11:49:53 -0300

    Primer borrador Home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const controller = {
 	index: (req, res) => {
-		res.send("Esta es la pagina principal!")
+		//res.send("Esta es la pagina principal!")
+		res.render('index', {
+			products: products
+		});
 	},
 };
 
