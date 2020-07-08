b2c349708b8fae9fdb6e075a06b5584b69b52915
jpcasa29 <juanpablocasa.lca@gmail.com>
2020-07-08 00:15:07 -0300

    agregando paginas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,11 +6,30 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
 	index: (req, res) => {
-		//res.send("Esta es la pagina principal!")
 		res.render('index', {
 			products: products
 		});
 	},
+	productoDetalle: (req, res) => {
+		res.render('producto_detalle', {
+			products: products
+		});
+	},
+	productoCarga: (req, res) => {
+		res.render('producto_carga', {
+			products: products
+		});
+	},
+	registro: (req, res) => {
+		res.render('registro', {
+			products: products
+		});
+	},
+	carrito: (req, res) => {
+		res.render('carrito', {
+			products: products
+		});
+	}
 };
 
 module.exports = controller;
--- src/routes/main.js
+++ src/routes/main.js
@@ -6,5 +6,11 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index); /* GET - home page */
+router.get('/producto/detalle/:id?', mainController.productoDetalle); /* GET - home page */
+router.get('/producto/carga', mainController.productoCarga); /* GET - home page */
+router.get('/registro', mainController.registro); /* GET - home page */
+router.get('/carrito', mainController.carrito); /* GET - home page */
+
+
 
 module.exports = router;
