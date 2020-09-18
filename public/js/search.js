6bf29e44e2c3989e9e1940130c96f30a728b1606
Tomas <tomijais@gmail.com>
2020-09-18 14:57:54 -0300

    search bar

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,34 @@
 window.addEventListener("load", function () {
   const $btn_search = document.querySelector(".button-buscador");
+  const $list = document.querySelector(".products-search");
   const $input_search = document.querySelector(".input-buscador");
-  const $ul = document.querySelector(".products-search");
 
   $input_search.addEventListener("input", function (e) {
-    console.log(e.target.value);
+
+    fetch("/admin/api")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (info) {
+        $list.innerHTML = "";
+        if (e.target.value == "") {
+          return ($list.innerHTML = "");
+        } else {
+          info.forEach((dato) => {
+            if (
+              dato.name
+                .toLowerCase()
+                .match(new RegExp(e.target.value.toLowerCase()))
+            ) {
+              $list.innerHTML +=
+                '<option value="' + dato.name + '">' + dato.name + "</option>";
+            }
+          });
+        }
+      });
+
+
+
+
   });
 });
