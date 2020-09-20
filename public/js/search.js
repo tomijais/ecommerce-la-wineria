14aeff45f2f469dd967e5719421f16947252153e
jpcasa29 <juanpablocasa.lca@gmail.com>
2020-09-20 19:05:11 -0300

    actualizando buscador

--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function () {
   const $btn_search = document.querySelector(".button-buscador");
   const $list = document.querySelector(".products-search");
   const $input_search = document.querySelector(".input-buscador");
+  const buscados = document.querySelector(".juancho")
 
   $input_search.addEventListener("input", function (e) {
     fetch("/admin/api")
@@ -9,10 +10,13 @@ window.addEventListener("load", function () {
         return response.json();
       })
       .then(function (info) {
+        buscados.setAttribute('hidden', true)
         $list.innerHTML = "";
         if (e.target.value == "") {
+          buscados.setAttribute('hidden', true)
           $list.innerHTML = "";
         } else {
+          buscados.removeAttribute('hidden')
           info.forEach((dato) => {
             if (
               dato.name
@@ -20,9 +24,13 @@ window.addEventListener("load", function () {
                 .match(new RegExp(e.target.value.toLowerCase()))
             ) {
               $list.innerHTML +=
-                `<li><a href='/producto/detalle/${dato.id}'>${dato.name}<a/></li>`
-            }
-          });
+                `<li style="margin: 0px;"><a class="encontrados" href='/producto/detalle/${dato.id}'>${dato.name}<a/></li>`
+            } 
+          })
+          
+          if($list.children.length == 0){
+            buscados.setAttribute('hidden', true)
+          }
         }
       });
   });
